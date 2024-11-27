0073219bd0834e3056ef4a4357ee630783800bf7
Nabil Boudra <nabilbboudra@gmail.com>
2024-11-27 21:23:01 +0100

    Add Search Event Handler

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,14 +4,16 @@ import JobListingCard from './components/ui/jobListingCard'
 import JobDescriptionCard from './components/ui/jobDescriptionCard'
 import TopBar from './components/ui/TopBar'
 import  getJobs from './api'
-import { useEffect, useState } from 'react';
+import { SetStateAction, useEffect, useState } from 'react';
 import SearchBar from './components/ui/SearchBar'
 import { Job } from './utils/types'
+import { isMatch } from './utils/helper'
 
 
 function App() {
   const [jobItems, setJobItems] = useState([]);
   const [selectedJobItem, setSelectedJobItem] = useState<Job | null>(null); 
+  const [searchString, setSearchString] = useState<String>("");
 
   useEffect(() => {
     const fetchData = async () => { 
@@ -21,6 +23,10 @@ function App() {
     };
     fetchData();
   }, []);
+
+  const handleSearchBarChange = (event: { target: { value: SetStateAction<String> } }) => {
+    setSearchString(event.target.value);
+  }
 
   const createHandleCardClick = (job: Job) => { 
     const handleCardClick = () => { 
@@ -34,11 +40,16 @@ function App() {
       <div className="border">
         <TopBar/>
          <div className="flex justify-center py-3">
-            <SearchBar/>
+            <SearchBar searchString={searchString} onChange={handleSearchBarChange}/>
           </div>
         <div className="flex h-full px-[20%]"> 
           <div className="w-1/3 mr-3" >
-            {jobItems.map(item => <div onClick={createHandleCardClick(item)}><JobListingCard jobItem={item}/></div>)}
+            {jobItems.filter(item => isMatch(item, searchString))
+                     .map(item => <div onClick={createHandleCardClick(item)}>
+                                      <JobListingCard jobItem={item}/>
+                                  </div>
+                          )
+            }
           </div>
             {(selectedJobItem && <JobDescriptionCard jobItem={selectedJobItem} className="w-2/3 h-[97vh] sticky inset-y-5"/>)}
          </div>
--- frontend/src/components/ui/SearchBar.tsx
+++ frontend/src/components/ui/SearchBar.tsx
@@ -1,7 +1,8 @@
+import { SetStateAction } from "react"
 import { Input } from "./input"
 import { Search } from 'lucide-react'
 
-export default function SearchBar() {
+export default function SearchBar({searchString, onChange} : {searchString: String, onChange: (event: { target: { value: SetStateAction<String> } }) => void}) {
   return (
     <div className="relative w-[25%]">
       <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-gray-500" />
@@ -9,6 +10,8 @@ export default function SearchBar() {
         type="search"
         placeholder="Search..."
         className="rounded-full pl-10 pr-4"
+        value={searchString?.toString()}
+        onChange={onChange}
       />
     </div>
   )
