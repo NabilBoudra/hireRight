64091bfd00902694e3ea8a7226fd68e68bd6770a
Nabil Boudra <nabilbboudra@gmail.com>
2024-12-10 19:34:13 +0100

    Remove Login Popup on Apply/Bookmark

--- frontend/src/components/ui/loginDiv.tsx
+++ frontend/src/components/ui/loginDiv.tsx
@@ -1,16 +1,23 @@
-import { handleLogin } from "@/auth";
+import { useToast } from "@/hooks/use-toast";
 import { useMsal } from "@azure/msal-react";
 import React from "react";
 
 function LoginDiv({ handleClick, children }: {handleClick: () => void, children: React.ReactNode}) { 
     const { instance } = useMsal(); 
-    
+    const { toast } = useToast(); 
+    const handleLoggedOutUserClick = (e) => {
+        e.preventDefault();
+        toast({
+            title: 'You must be logged in to use this feature.'
+        })
+    }; 
+
     return ( instance.getActiveAccount()?
                 <div onClick={ handleClick }> 
                     {children}
                 </div>
             : 
-                <div onClick={ handleLogin }>
+                <div onClick={ handleLoggedOutUserClick }>
                     {children}
                 </div>
     ); 
